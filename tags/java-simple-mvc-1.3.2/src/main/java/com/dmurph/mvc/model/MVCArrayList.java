2996d3be50444e1568153f32d3135901a3c37da6
toucansam99 <toucansam99@909d88fa-14fa-9246-88e6-ba1adabc3c47>
2010-08-04 08:06:34 +0000

    [maven-release-plugin]  copy for tag java-simple-mvc-1.3.2

---
+++ tags/java-simple-mvc-1.3.2/src/main/java/com/dmurph/mvc/model/MVCArrayList.java
@@ -0,0 +1,390 @@
+/**
+ * Copyright (c) 2010 Daniel Murphy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/**
+ * Created at 2:47:16 PM, Apr 5, 2010
+ */
+package com.dmurph.mvc.model;
+
+import java.beans.IndexedPropertyChangeEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+
+import com.dmurph.mvc.ICloneable;
+import com.dmurph.mvc.IDirtyable;
+import com.dmurph.mvc.IModel;
+import com.dmurph.mvc.IRevertible;
+
+/**
+ * A full mvc implementation of an {@link ArrayList}.  Supports all operations in {@link ICloneable}, {@link IDirtyable},
+ * and {@link IRevertible}.  Also fires property change events for the size of the array ({@link #SIZE}) and the dirty value
+ * ({@link IModel#DIRTY} - which will also fire if any children do), and if an element in the array changed ({@link #CHANGED})
+ * or added ({@link #ADDED}.<br/>
+ * <br/>
+ * This class also will forward all calls to it's
+ * members if implement the associated interface.  For example, if {@link #revertChanges()} is called, then, after
+ * reverting any changes to this model, it will call {@link IRevertible#revertChanges()} on any property
+ * that is {@link IRevertible}.  This can get dangerous if your property tree goes in a loop (you'll 
+ * get infinite calls).  In that case override {@link #cloneImpl(Object)}, {@link #revertChangesImpl(IRevertible)},
+ * {@link #isDirtyImpl(IDirtyable)}, {@link #setDirtyImpl(IDirtyable, boolean)}, or {@link #saveChangesImpl(IRevertible)}
+ * to prevent this.<br/>
+ * <br/>
+ * All the operations are also synchronized, as most MVC implementations are multithreaded, and any 
+ * @author Daniel Murphy
+ */
+public class MVCArrayList<E extends Object> extends ArrayList<E> implements IModel, ICloneable, IDirtyable, IRevertible {
+	private static final long serialVersionUID = 4890270966369581329L;
+	
+	/**
+	 * Array size property name for listening to property change events
+	 * @see #addPropertyChangeListener(PropertyChangeListener)
+	 */
+	public static final String SIZE = "ARRAY_LIST_SIZE";
+	
+	/**
+	 * Not exactly a property, but the name of the property when an element
+	 * is removed from the array.  The {@link IndexedPropertyChangeEvent#getIndex()}
+	 * cooresponds to the index that was removed, and is -1 if {@link #remove(Object)}
+	 * is called (we don't know the index).
+	 */
+	public static final String REMOVED = "ARRAY_LIST_REMOVED";
+	
+	/**
+	 * Not exactly a property, but the name of the property when an element
+	 * is added or inserted into the array.  This fires an {@link IndexedPropertyChangeEvent}.
+	 */
+	public static final String ADDED = "ARRAY_LIST_ADDED";
+	/**
+	 * A value in the array was changed.  This fires an
+	 * {@link IndexedPropertyChangeEvent}.
+	 * @see #addPropertyChangeListener(PropertyChangeListener)
+	 */
+	public static final String CHANGED = "ARRAY_LIST_CHANGED";
+	
+	private boolean dirty = false;
+	
+	private final ArrayList<E> saved = new ArrayList<E>();
+	private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+;
+	
+	private final PropertyChangeListener childPropertyChangeListener = new PropertyChangeListener() {
+		public void propertyChange(PropertyChangeEvent argEvt) {
+			if(argEvt.getPropertyName().equals(IModel.DIRTY)){
+				if(argEvt.getNewValue() == Boolean.TRUE){
+					propertyChangeSupport.firePropertyChange(argEvt);
+				}
+			}
+		}
+	};
+    
+    private void addListener(Object argObject){
+		if(argObject instanceof IModel){
+			((IModel) argObject).addPropertyChangeListener(childPropertyChangeListener);
+		}
+	}
+	
+	private void removeListener(Object argObject){
+		if(argObject instanceof IModel){
+			((IModel) argObject).removePropertyChangeListener(childPropertyChangeListener);
+		}
+	}
+    
+	@Override
+	public synchronized boolean add(E e) {
+		boolean ret = super.add(e);
+		addListener(e);
+		propertyChangeSupport.fireIndexedPropertyChange(ADDED, size()-1, null, e);
+		firePropertyChange(SIZE, size() - 1, size());
+		boolean old = dirty;
+		dirty = true;
+		firePropertyChange(DIRTY, old, dirty);
+		return ret;
+	}
+	
+	public synchronized void add(int index, E element) {
+		super.add(index, element);
+		addListener(element);
+		propertyChangeSupport.fireIndexedPropertyChange(ADDED, index, null, element);
+		firePropertyChange(SIZE, size() - 1, size());
+		boolean old = dirty;
+		dirty = true;
+		firePropertyChange(DIRTY, old, dirty);
+	}
+	
+	private final ArrayList<E> temp = new ArrayList<E>();
+	
+	@Override
+	public synchronized void clear() {
+		if(size() > 0){
+			int oldSize = size();
+			temp.clear();
+			temp.addAll(this);
+			super.clear();
+			for(int i=0; i<temp.size(); i++){
+				E e = temp.get(i);
+				removeListener(e);
+				propertyChangeSupport.fireIndexedPropertyChange(REMOVED, i, e, null);
+			}
+			firePropertyChange(SIZE, oldSize, 0);
+			boolean old = dirty;
+			dirty = true;
+			firePropertyChange(DIRTY, old, dirty);
+		}
+	}
+	
+	@Override
+	public synchronized E remove(int index) {
+		E ret = super.remove(index);
+		removeListener(ret);
+		propertyChangeSupport.fireIndexedPropertyChange(REMOVED, index, ret, null);
+		firePropertyChange(SIZE, size() + 1, size());
+		boolean old = dirty;
+		dirty = true;
+		firePropertyChange(DIRTY, old, dirty);
+		return ret;
+	}
+	
+	@Override
+	public synchronized boolean remove(Object o) {
+		boolean ret = super.remove(o);
+		if(ret){
+			removeListener(o);
+			propertyChangeSupport.fireIndexedPropertyChange(REMOVED, -1, o, null);
+			firePropertyChange(SIZE, size() + 1, size());
+			boolean old = dirty;
+			dirty = true;
+			firePropertyChange(DIRTY, old, dirty);
+		}
+		return ret;
+	}
+	
+	@Override
+	public synchronized E set(int index, E element) {
+		E ret = super.set(index, element);
+		removeListener(ret);
+		addListener(element);
+		propertyChangeSupport.fireIndexedPropertyChange(CHANGED, index, ret, element);
+		boolean old = dirty;
+		dirty = true;
+		firePropertyChange(DIRTY, old, dirty);
+		return ret;
+	}
+	
+	// do shallow clone, need to keep object references
+	private void setFromSaved(){
+		clear();
+		for(E e: saved){
+			add(e);
+		}
+	}
+	
+	// do shallow clone, need to keep object references
+	private void setToSaved(){
+		saved.clear();
+		for(E e: this){
+			saved.add(e);
+		}
+	}
+	
+	 /**
+	 * @see com.dmurph.mvc.IModel#addPropertyChangeListener(java.beans.PropertyChangeListener)
+	 */
+    public void addPropertyChangeListener(PropertyChangeListener argListener) {
+        propertyChangeSupport.addPropertyChangeListener(argListener);
+    }
+
+    /**
+	 * @see com.dmurph.mvc.IModel#removePropertyChangeListener(java.beans.PropertyChangeListener)
+	 */
+    public void removePropertyChangeListener(PropertyChangeListener argListener) {
+        propertyChangeSupport.removePropertyChangeListener(argListener);
+    }
+
+    private void firePropertyChange(String argPropertyName, Object argOldValue, Object argNewValue) {
+        propertyChangeSupport.firePropertyChange(argPropertyName, argOldValue, argNewValue);
+    }
+    
+	
+	/**
+	 * Clones from another {@link ArrayList}, if the values are {@link ICloneable}, then
+	 * they will be cloned to this one.  Otherwise it's a shallow copy (just sets the same values).
+	 * @param argOther an {@link ArrayList}
+	 * @see com.dmurph.mvc.ICloneable#cloneFrom(com.dmurph.mvc.ICloneable)
+	 */
+	@SuppressWarnings("unchecked")
+	public synchronized void cloneFrom( ICloneable argOther) {
+		MVCArrayList<E> other = (MVCArrayList<E>) argOther;
+		clear();
+		for(E e : other){
+			add(cloneImpl(e));
+		}
+		saved.clear();
+		for(E e : other.saved){
+			saved.add(cloneImpl(e));
+		}
+		this.dirty = other.dirty;
+	}
+	
+	/**
+	 * Default just calls {@link ICloneable#clone}, but override
+	 * to protect against loops (if the property tree goes in a loop).
+	 * @param argObject
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	protected E cloneImpl(E argObject){
+		if(argObject instanceof ICloneable){
+			return (E) ((ICloneable) argObject).clone();
+		}else{
+			return argObject;
+		}
+	}
+
+	/**
+	 * Clones this object to another {@link MVCArrayList}.  If the array values
+	 * are also {@link ICloneable}, then they will be cloned as well.  If not, the values
+	 * are just set (shallow copy).
+	 * @see java.util.ArrayList#clone()
+	 */
+	@Override
+	public synchronized ICloneable clone(){
+		MVCArrayList<E> other = new MVCArrayList<E>();
+		other.cloneFrom(this);
+		return other;
+	}
+
+	/**
+	 * Also checks to see if elements in this
+	 * array are dirty, if any are {@link IDirtyable}.
+	 * @see com.dmurph.mvc.IDirtyable#isDirty()
+	 */
+	public synchronized boolean isDirty() {
+		if(dirty){
+			return true;
+		}
+		for(E e : this){
+			if(e instanceof IDirtyable){
+				if(isDirtyImpl(e)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Default just calls {@link IDirtyable#isDirty()}, but override
+	 * to protect against loops (if the property tree goes in a loop).
+	 * @return
+	 */
+	protected boolean isDirtyImpl(E argE){
+		if(argE instanceof IDirtyable){
+			if(((IDirtyable) argE).isDirty()){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Sets the dirty variable and, if argDirty is false,
+	 * then will call {@link IDirtyable#setDirty(boolean)} on
+	 * all {@link IDirtyable} objects in this array.
+	 * @see com.dmurph.mvc.IDirtyable#setDirty(boolean)
+	 */
+	public synchronized boolean setDirty( boolean argDirty) {
+		boolean oldDirty = dirty;
+		dirty = argDirty;
+		if(!dirty){
+			for(E e: this){
+				setDirtyImpl(e, false);
+			}
+		}
+		firePropertyChange(DIRTY, oldDirty, dirty);
+		return oldDirty;
+	}
+	
+	/**
+	 * Default just calls {@link IDirtyable#setDirty(boolean)}, but override
+	 * to protect against loops (if the property tree goes in a loop).
+	 * @param argE
+	 * @param argDirty
+	 */
+	protected void setDirtyImpl(E argE, boolean argDirty){
+		if(argE instanceof IDirtyable){
+			((IDirtyable) argE).setDirty(argDirty);
+		}
+	}
+
+	/**
+	 * Also calls {@link IRevertible#revertChanges()} on all
+	 * objects in the reverted array that are {@link IRevertible}.
+	 * @see com.dmurph.mvc.IRevertible#revertChanges()
+	 */
+	public synchronized boolean revertChanges() {
+		if(!isDirty()){
+			return false;
+		}
+		setFromSaved();
+		for(E e: this){
+			revertChangesImpl(e);
+		}
+		return true;
+	}
+	
+	/**
+	 * Default just calls {@link IRevertible#revertChanges()}, but override
+	 * to protect against loops (if the property tree goes in a loop).
+	 */
+	protected void revertChangesImpl(E argE){
+		if(argE instanceof IRevertible){
+			((IRevertible) argE).revertChanges();
+		}
+	}
+
+	/**
+	 * Also calls {@link IRevertable#saveChanges()} on all
+	 * objects in the reverted array that are {@link IRevertible}.
+	 * @see com.dmurph.mvc.IRevertible#saveChanges()
+	 */
+	public synchronized boolean saveChanges() {
+		if(!isDirty()){
+			return false;
+		}
+		setToSaved();
+		for(E e: this){
+			saveChangesImpl(e);
+		}
+		return true;
+	}
+	
+	/**
+	 * Default just calls {@link IRevertible#saveChanges()}, but override
+	 * to protect against loops (if the property tree goes in a loop).
+	 */
+	protected void saveChangesImpl(E argE){
+		if(argE instanceof IRevertible){
+			((IRevertible) argE).saveChanges();
+		}
+	}
+}
---
+++ tags/java-simple-mvc-1.3.2/src/main/java/com/dmurph/mvc/monitor/EventTableModel.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2010 Daniel Murphy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/**
+ * Created at Aug 4, 2010, 2:03:17 AM
+ */
+package com.dmurph.mvc.monitor;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import com.dmurph.mvc.MVCEvent;
+import com.dmurph.mvc.ObjectEvent;
+import com.dmurph.mvc.monitor.EventMonitor.EventType;
+
+/**
+ * @author daniel
+ *
+ */
+public class EventTableModel extends AbstractTableModel {
+	private static final long serialVersionUID = 1L;
+	
+	private final ArrayList<LogEntry> events = new ArrayList<LogEntry>();
+	private int nextMessageIndex = 0;
+	private final int maxMessagesLogged;
+	
+	public EventTableModel(int argMaxMessagesLogged){
+		maxMessagesLogged = argMaxMessagesLogged;
+	}
+	
+	public void logEvent(MVCEvent argEvent, EventType argType){
+		if(maxMessagesLogged == EventMonitor.LOG_ALL_MESSAGES){
+			LogEntry entry = new LogEntry();
+			entry.populate(argEvent, argType);
+			events.add(entry);
+			return;
+		}
+		if(events.size() == maxMessagesLogged){
+			events.get(nextMessageIndex).populate(argEvent, argType);
+			nextMessageIndex = (nextMessageIndex+1)%maxMessagesLogged;
+			return;
+		}
+		LogEntry entry = new LogEntry();
+		entry.populate(argEvent, argType);
+		events.add(entry);
+		fireTableDataChanged();
+	}
+	
+	final String[] columns = {
+			"Event Key", "Event Class", "Event Value", "Never Recieved", "Thread"
+	};
+	
+	/**
+	 * @see javax.swing.table.AbstractTableModel#getColumnName(int)
+	 */
+	@Override
+	public String getColumnName(int argColumn) {
+		return columns[argColumn];
+	}
+	
+	/**
+	 * @see javax.swing.table.TableModel#getColumnCount()
+	 */
+	public int getColumnCount() {
+		return columns.length;
+	}
+	
+	/**
+	 * @see javax.swing.table.TableModel#getRowCount()
+	 */
+	public int getRowCount() {
+		return events.size();
+	}
+	
+	/**
+	 * @see javax.swing.table.TableModel#getValueAt(int, int)
+	 */
+	public Object getValueAt(int argRowIndex, int argColumnIndex) {
+		if(maxMessagesLogged != EventMonitor.LOG_ALL_MESSAGES){
+			argRowIndex += nextMessageIndex;
+			argRowIndex %= maxMessagesLogged;
+		}
+		LogEntry entry = events.get(argRowIndex);
+		switch(argColumnIndex){
+			case 0:
+				return entry.eventKey;
+			case 1:
+				return entry.eventClass;
+			case 2:
+				return (entry.eventValue != null)?entry.eventValue : "";
+			case 3:
+				return (entry.type == EventType.NO_LISTENERS)? "*" : null;
+			case 4:
+				return entry.threadName;
+			default:
+				return "?";
+		}
+	}
+	
+	private static class LogEntry{
+		String eventClass;
+		String eventKey;
+		String eventValue;
+		String threadName;
+		EventType type;
+		
+		public void populate(MVCEvent argEvent, EventType argType){
+			type = argType;
+			eventClass = argEvent.getClass().getSimpleName();
+			eventKey = argEvent.key;
+			eventValue = (argEvent instanceof ObjectEvent<?>)? ((ObjectEvent<?>)argEvent).getValue().toString() : null;
+			threadName = Thread.currentThread().getName();
+		}
+	}
+}
